3aff59934d291a1acd8cd26159d2198c1ea460f4
Hureru <121702350+Hureru@users.noreply.github.com>
2025-07-29 23:29:08 +0800

    build(services): 更新安全配置和依赖
    - iam-service/pom.xml 的 Spring Boot Security Starter 和 Spring Security OAuth2 Authorization Server 依赖移动到 services/pom.xml
    - 在 product_artisan-service 中添加 OAuth2 资源服务器配置

--- services/product_artisan-service/src/main/java/com/hureru/product_artisan/config/SecurityConfig.java
+++ services/product_artisan-service/src/main/java/com/hureru/product_artisan/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.hureru.product_artisan.config;
 
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -22,6 +27,7 @@ public class SecurityConfig {
                         .permitAll()
                         .anyRequest().authenticated()
                 )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
