11ef804b8dcae4994508753bfce82af52fcfd9b3
Hureru <121702350+Hureru@users.noreply.github.com>
2025-07-30 23:55:16 +0800

    feat(security): 允许未认证用户访问 /api/v1/public/** 路径
    - 增加了对 /api/v1/public/** 路径的访问权限，允许未认证用户访问

--- services/product_artisan-service/src/main/java/com/hureru/product_artisan/config/SecurityConfig.java
+++ services/product_artisan-service/src/main/java/com/hureru/product_artisan/config/SecurityConfig.java
@@ -1,15 +1,11 @@
 package com.hureru.product_artisan.config;
 
-import com.nimbusds.jose.jwk.source.JWKSource;
-import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -23,7 +19,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/artisan")
+                        .requestMatchers("/api/v1/artisan",
+                                "/api/v1/public/**")
                         .permitAll()
                         .anyRequest().authenticated()
                 )
