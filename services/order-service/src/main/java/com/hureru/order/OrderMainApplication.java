ae290f7377a4aa9e7fbef80ed2a420ac8f67adce
Hureru <121702350+Hureru@users.noreply.github.com>
2025-08-15 16:31:04 +0800

    feat(order-service): 添加 Feign 和 Nacos 支持
    - 在 OrderMainApplication 类上添加了 @EnableFeignClients 注解，启用 Feign 客户端支持
    - 添加了 @EnableDiscoveryClient 注解，使应用能够使用 Nacos 服务发现

--- services/order-service/src/main/java/com/hureru/order/OrderMainApplication.java
+++ services/order-service/src/main/java/com/hureru/order/OrderMainApplication.java
@@ -2,10 +2,14 @@ package com.hureru.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author zheng
  */
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OrderMainApplication {
     public static void main(String[] args) {
