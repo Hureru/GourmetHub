3fc8e7d8d55ebe2fe65d4c23999342547ce1f49f
Hureru <121702350+Hureru@users.noreply.github.com>
2025-08-17 00:41:03 +0800

    feat(gateway): 添加网关服务并配置路由
    - 新增 gateway 模块，实现网关服务功能
    - 添加 Nacos 配置，用于服务注册和发现
    - 配置全局 CORS，支持跨域请求
    - 启用路由发现功能，自动识别服务路由

---
+++ gateway/src/main/java/com/hureru/gateway/GatewayMainApplication.java
@@ -0,0 +1,15 @@
+package com.hureru.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author zheng
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayMainApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(GatewayMainApplication.class, args);
+    }
+}
